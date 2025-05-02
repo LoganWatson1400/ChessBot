638ef5dfe7098832e4b86e369f898a54f92feb9e
HawkerX <jackharmer5@gmail.com>
2025-05-02 13:57:13 -0500

    feat: added uci documentation and main

---
+++ src/engine.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/*
+ * UCI PROTOCAL
+ * STDIN	STDOUT
+ * -uci		uciok
+ * -isready	readyok
+ * -ucinewgame  <make new game>
+ * -position    <setup position>
+ * -go		<start calculating>
+ *
+ * Return best move whenever after go 
+ * 		OR
+ * -stop	bestmove <move>
+ *
+ * -quit	<shutdown engine>
+ *
+*/
+int main(){
+	while(1){
+
+	}
+	return 0;
+}
